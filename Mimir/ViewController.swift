ab9b4c23168c40f9c57f048b473a8da8c2f927cd
Student <sandret579@c332-ml-mc1-260.omahaps.ops.org>
2017-03-08 08:37:48 -0600

    Initial Commit

---
+++ Mimir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mimir
+//
+//  Created by Travis J Andrews on 3/8/17.
+//  Copyright © 2017 IT North. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
